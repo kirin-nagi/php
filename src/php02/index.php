e476773e98d57880eeb8b4b4585312b2c109c9ab
なまえ <taro.yamada@gmail.com>
2025-02-13 19:33:39 +0900

    2025/02/13

--- src/php02/index.php
+++ src/php02/index.php
@@ -14,31 +14,30 @@
         <div class="header__inner">
             <a class="header__logo" href="/php02/index.php">
                 World Clock
-</a>
-</div>
-</header>
+            </a>
+        </div>
+    </header>
 <main>
     <div class="search-form__content">
         <div class="search-form__heading">
             <h2>日本の世界の時間を比較<h2>
-</div>
-    <from class="search-form" action="result.php" method="get">
+        </div>
+    <form class="search-form" action="result.php" method="get">
         <div class="search-form__item">
-        <checkbox class="search-form__item-select" name="city">
-        <label><input type="checkbox" name="[シドニー]"  value="シドニー">シドニー</label>
-        <label><input type="checkbox" name="[上海]" value="上海">上海</label>
-        <label><input type="checkbox" name="[モスクワ]" value="モスクワ">モスクワ</label>
-        <label><input type="checkbox" name="[ロンドン]" value="ロンドン">ロンドン</label>
-        <label><input type="checkbox" name="[ヨハネスブルグ]" value="ヨハネスブルグ">ヨハネスブルグ</label>
-        <label><input type="checkbox" name="[ニューヨーク]" value="ニューヨーク">ニューヨーク</label>
-        </checkbox>
+        <div class="search-form__item-select" name="city">
+        <label><input type="checkbox" name="シドニー"  value="シドニー">シドニー</label>
+        <label><input type="checkbox" name="上海" value="上海">上海</label>
+        <label><input type="checkbox" name="モスクワ" value="モスクワ">モスクワ</label>
+        <label><input type="checkbox" name="ロンドン" value="ロンドン">ロンドン</label>
+        <label><input type="checkbox" name="ヨハネスブルグ" value="ヨハネスブルグ">ヨハネスブルグ</label>
+        <label><input type="checkbox" name="ニューヨーク" value="ニューヨーク">ニューヨーク</label>
+        </div>
         </div>
         <div class="search-form__button">
         <button class="search-form__button-submit" type="submit">検索</button>
         </div>
-    </from>
-</div>
-</div>
+    </form>
+    </div>
 </main>
 </body>
 
